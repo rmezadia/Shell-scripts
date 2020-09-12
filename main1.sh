f185937989246d811ee1e9aea3f7de3e97a5d5f6
Ricardo José Meza Díaz <rmezadia@alumnos.unex.es>
2020-09-12 20:48:20 +0000

    khk

--- main1.sh
+++ main1.sh
@@ -1,4 +1,7 @@
-#Se mete en el escritorio, muestra el contenido, lo guarda en un txt y lo abre con un procesador de textos.
+#Se mete en el escritorio,crea una carpeta, el contenido lo guarda en un txt y lo abre con un procesador de textos.
 cd Desktop 
 ls >> contenidoescritorio.txt
+mkdir infodirectorio
+contenidoescritorio.txt >> infodirectorio
+ls infodirectorio
 open contenidoescritorio.txt
