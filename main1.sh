13dda80f9bf34c5e5a3338f634cbcebd50461f3f
Ricardo José Meza Díaz <rmezadia@alumnos.unex.es>
2020-09-13 13:21:31 +0200

    Add files via upload

--- main1.sh
+++ main1.sh
@@ -1,7 +1,9 @@
 #Se mete en el escritorio,crea una carpeta, el contenido lo guarda en un txt y lo abre con un procesador de textos.
-cd Desktop 
-ls >> contenidoescritorio.txt
-mkdir infodirectorio
-contenidoescritorio.txt >> infodirectorio
-ls infodirectorio
-open contenidoescritorio.txt
+cd Desktop        						#Entra en el directorio Desktop.                                         
+ls >> contenidoescritorio.txt			#Su contenido lo guarda en un txt.
+mkdir infodirectorio                   	#Crea un directorio nuevo.
+chmod 755 infodirectorio				#Pone priviliegios al usuario de u+x.
+chmod 755 contenidoescritorio.txt  		#Pone priviliegios al usuario de u+x.
+mv contenidoescritorio.txt /Users/ricardomezadiaz/Desktop/infodirectorio #el fichero txt lo mueve a la carpeta.
+ls infodirectorio						#Entra en el directorio creado.
+open contenidoescritorio.txt			#Abre el txt con el proc de estos.
